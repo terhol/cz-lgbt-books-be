807d19c997bc7f0a3356963188bb20648d89249a
Tereza Holm <holm.tereza@gmail.com>
2020-01-05 20:46:23 +0100

    Pagination added, basic sorting

--- src/main/java/czdbdk/dbdkbe/controllers/BookController.java
+++ src/main/java/czdbdk/dbdkbe/controllers/BookController.java
@@ -3,11 +3,16 @@ package czdbdk.dbdkbe.controllers;
 import czdbdk.dbdkbe.models.Book;
 import czdbdk.dbdkbe.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Tereza Holm
@@ -15,17 +20,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-
     @Autowired
     private BookRepository bookRepository;
+    private Map<String, String> orderByMap = prepareMap();
+
 
     @GetMapping
-    public Iterable<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public Iterable<Book> getAllBooks(
+            @RequestParam(defaultValue = "title") String orderBy,
+            @RequestParam(defaultValue = "ASC") Sort.Direction order,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "-1") Integer size
+    ) {
+        if(size == -1){
+            size = Math.toIntExact(bookRepository.count());
+        }
+        orderBy = orderByMap.getOrDefault(orderBy, "title");
+
+        Pageable pageable = PageRequest.of(page,size, Sort.by(orderBy));
+
+        return bookRepository.findAll(pageable);
     }
 
     @GetMapping("/total")
-    public long getNumberOfBooks(){
+    public long getNumberOfBooks() {
         return bookRepository.count();
+    }
+
+    private Map<String, String> prepareMap(){
+        Map<String, String> orderByMap = new HashMap<>();
+        orderByMap.put("TITLE", "title");
+        orderByMap.put("YEAR_OF_ISSUE", "yearOfIssue");
+        orderByMap.put("DATE_OF_ADDITION", "dateOfAdition");
+        return orderByMap;
     }
 }
--- src/main/java/czdbdk/dbdkbe/repositories/BookRepository.java
+++ src/main/java/czdbdk/dbdkbe/repositories/BookRepository.java
@@ -1,6 +1,8 @@
 package czdbdk.dbdkbe.repositories;
 
 import czdbdk.dbdkbe.models.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BookRepository extends PagingAndSortingRepository<Book,Long> {
+
+    Page<Book> findAll(Pageable pageable);
 }
