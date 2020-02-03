3d10ca067d96da33af87d6c37846db91c5cd59e9
Tereza Holm <holm.tereza@gmail.com>
2020-02-03 15:22:14 +0100

    Fixed sorting bug

--- src/main/java/czdbdk/dbdkbe/controllers/BookController.java
+++ src/main/java/czdbdk/dbdkbe/controllers/BookController.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,7 +55,7 @@ public class BookController {
     ) {
         orderBy = orderByMap.getOrDefault(orderBy, "dateOfAddition");
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by(order, orderBy));
+        Pageable pageable = PageRequest.of(page, size, Sort.by(order, orderBy).and(Sort.by(order, "id")));
 
         return bookRepository.findAll(pageable).getContent();
     }
